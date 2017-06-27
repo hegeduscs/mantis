2349865e97709879649dc569eb38f6401828c27b
Umlauf Zoltán <zoltan.umlauf@gmail.com>
2017-06-27 14:15:35 +0200

    linear regression commit

--- R/247truck.R
+++
@@ -1,5 +0,0 @@
-truck247_logs<-subset(important_replacements,important_replacements$identifier=="516213C00247")
-truck247_all_logs<-subset(still,still$identifier=="516213C00247" & !is.na(still$Concatenate.Material..for))
-
-truck247_all_logs<-subset(still,still$identifier=="516213C00247" & !is.na(still$metamachinerrorcode))
-table(truck247_all_logs$metamachinerrorcode)
