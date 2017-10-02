15f92f6f0be95fd9f1b602b7b9af214ed21bdc69
vasy <007vasy@gmail.com>
2017-10-02 10:45:19 +0200

    factoring fast.slow and wl.wol done

--- exploratory_data_analysis.R
+++ exploratory_data_analysis.R
@@ -5,8 +5,8 @@ library(purrr)
 library(plyr)
 library(lubridate)
 library(zoo)
-library(reshape2)
 library(ggplot2)
+library(stringr)
 
 #PC
 setwd("/home/vasy/RStudioProjects/still_github/cleaned_files/cleaned_files/")
@@ -28,19 +28,21 @@ for(file_name_i in list.files())
   }
 }
 
-df_container$fingerprint_type = factor(df_container$fingerprint_type)
-levels(df_container$fingerprint_type)
-df_container$time = hms(df_container$time)
+df_container = df_container %>%
+  mutate(fast.slow = str_detect(fingerprint_type,"fast")) %>%
+  mutate(wl.wol = str_detect(fingerprint_type,"_wl_")) %>%
+  mutate(time = hms(time)) %>%
+  mutate(fingerprint_type = factor(fingerprint_type))
 glimpse(df_container)
 
-hist(df_container$Speed_Steering_wheel_U.min)
-#Steering_angle_angle
-hist(df_container$Steering_angle_angle)
+# hist(df_container$Speed_Steering_wheel_U.min)
+# #Steering_angle_angle
+# hist(df_container$Steering_angle_angle)
+
 
 #GGSAVE ?
-ggplot(df_container,aes(x = Speed_Steering_wheel_U.min))+geom_density() + facet_wrap(~fingerprint_type, scales = 'free_x')
-ggplot(df_container,aes(x = Speed_Steering_wheel_U.min))+geom_density() + facet_wrap(~fingerprint_type, scales = 'free_x')
-ggplot(df_container,aes(Speed_Drivemotor_2_U.min,Torque_Drivemotor_2_Nm))+geom_point(alpha = 0.1, shape = 21, size = 1.5) + facet_wrap(~fingerprint_type, scales = 'free_x')
-ggplot(df_container,aes(as.numeric(time),Steering_angle_angle))+geom_point(alpha = 0.4, shape = 21, size = 1.5) + facet_wrap(~fingerprint_type, scales = 'free_x')
-
-
+# ggplot(df_container,aes(x = Speed_Steering_wheel_U.min))+geom_density() + facet_wrap(~fingerprint_type, scales = 'free_x')
+# ggplot(df_container,aes(x = Speed_Steering_wheel_U.min))+geom_density() + facet_wrap(~fingerprint_type, scales = 'free_x')
+# ggplot(df_container,aes(Speed_Drivemotor_2_U.min,Torque_Drivemotor_2_Nm))+geom_point(alpha = 0.1, shape = 21, size = 1.5) + facet_wrap(~fingerprint_type, scales = 'free_x')
+# ggplot(df_container,aes(as.numeric(time),Steering_angle_angle))+geom_point(alpha = 0.4, shape = 21, size = 1.5) + facet_wrap(~fingerprint_type, scales = 'free_x')
+#ggplot(df_container,aes(Speed_Drivemotor_2_U.min,Torque_Drivemotor_2_Nm,color = fast.slow,fill = wl.wol))+geom_point(alpha = 0.1, shape = 21, size = 1.5) + facet_wrap(~fingerprint_type, scales = 'free_x')
