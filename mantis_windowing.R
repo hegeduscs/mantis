bd7816605b048704073008dca08f236dc0788e08
vasy <007vasy@gmail.com>
2017-10-19 11:29:57 +0200

    windowing proof of concept roll_mean

--- mantis_windowing.R
+++ mantis_windowing.R
@@ -8,7 +8,7 @@ library(lubridate)
 library(zoo)
 library(ggplot2)
 library(stringr)
-library(GGally)
+library(RcppRoll)
 
 #PC
 setwd("/home/vasy/RStudioProjects/still_github/cleaned_files/cleaned_files/")
@@ -18,14 +18,13 @@ tdf1 = read_csv("FastTrackEight_wl_slow.csv")
 tdf2 = read_csv("FastTrackEight_wol_slow.csv")
 tdfsum = bind_rows(tdf1,tdf2)
 
-tdfsum %>%
+tdfsum = tdfsum %>%
   colwise(na.locf)() %>%
   summary()
 summary(colwise(na.locf)(tdfsum))
 
 
-tdf = ddply(tdf, .(fx_code), function(x) replace(x, TRUE, lapply(x, na.locf0, TRUE)))
-summary(tdf)
+summary(tdfsum)
 tdf = mutate(tdf,time = hms(time))
 head(tdf$time)
 max(tdf$time)
@@ -35,4 +34,9 @@ names(tdfsum)
 ggplot(tdf1,aes(x = as.numeric(time),y = Pressure_Hydraulic_main_mast_bar)) + geom_point(alpha = 0.8, shape = 21, size = 1.5)
 
 #weight search
-tdfsum = mutate(tdfsum,is.weight = rollapply(Pressure_Hydraulic_main_mast_bar,width = 10, mean) > 50)
+
+tdfsum = mutate(tdf1,weight_mean = roll_mean(Pressure_Hydraulic_main_mast_bar,10,fill = numeric(0),align = "center"))
+length(tdfsum$Pressure_Hydraulic_main_mast_bar)
+length(roll_mean(tdfsum$Pressure_Hydraulic_main_mast_bar,10,fill = numeric(0),align = "center"))
+#coerce nulls
+
