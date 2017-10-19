18ba4dc3d0d00f4c9e5d403b5f78d498b1745498
vasy <007vasy@gmail.com>
2017-10-19 18:44:13 +0200

    is.weight indicator, direction changed indicator

--- mantis_windowing.R
+++ mantis_windowing.R
@@ -25,13 +25,13 @@ summary(colwise(na.locf)(tdfsum))
 
 
 summary(tdfsum)
-tdf = mutate(tdf,time = hms(time))
-head(tdf$time)
-max(tdf$time)
-min(tdf$time)
-tail(tdf$time)
-names(tdfsum)
-ggplot(tdf1,aes(x = as.numeric(time),y = Pressure_Hydraulic_main_mast_bar)) + geom_point(alpha = 0.8, shape = 21, size = 1.5)
+# tdf = mutate(tdf,time = hms(time))
+# head(tdf$time)
+# max(tdf$time)
+# min(tdf$time)
+# tail(tdf$time)
+# names(tdfsum)
+# ggplot(tdf1,aes(x = as.numeric(time),y = Pressure_Hydraulic_main_mast_bar)) + geom_point(alpha = 0.8, shape = 21, size = 1.5)
 
 #windowing search
 
@@ -40,15 +40,24 @@ w_width = 10
 if(w_width%%2!=0)
   print("Must be even!")
 
-tdfsum = mutate(
+#is.weight on the truck
+tdf_attributes = mutate(
   tdfsum,
   weight_mean = c(rep(0,w_width/2),
                   roll_mean(Pressure_Hydraulic_main_mast_bar,w_width,fill = numeric(0),align = "center"),
                   rep(0,w_width/2-1)
-                  )
+                  ),
+  is.weight = weight_mean > 50 #contans from plots
                 )
 
-length(tdfsum$Pressure_Hydraulic_main_mast_bar)
-length(roll_mean(tdfsum$Pressure_Hydraulic_main_mast_bar,10,fill = numeric(0),align = "center"))
+#changing direction
+tdf_attributes = mutate(tdf_attributes,
+                        direction_changed = Speed_Drivemotor_2_U.min
+                        )
+
+
+summary(tdf_attributes)
+# length(tdfsum$Pressure_Hydraulic_main_mast_bar)
+# length(roll_mean(tdfsum$Pressure_Hydraulic_main_mast_bar,10,fill = numeric(0),align = "center"))
 #coerce nulls
 
