130f3bdf2288ff3404c30b2d8edb4ffa714caa04
vasy <007vasy@gmail.com>
2017-10-19 18:22:25 +0200

    moving mean pilot tested

--- mantis_windowing.R
+++ mantis_windowing.R
@@ -19,8 +19,8 @@ tdf2 = read_csv("FastTrackEight_wol_slow.csv")
 tdfsum = bind_rows(tdf1,tdf2)
 
 tdfsum = tdfsum %>%
-  colwise(na.locf)() %>%
-  summary()
+  colwise(na.locf)() 
+
 summary(colwise(na.locf)(tdfsum))
 
 
@@ -33,9 +33,21 @@ tail(tdf$time)
 names(tdfsum)
 ggplot(tdf1,aes(x = as.numeric(time),y = Pressure_Hydraulic_main_mast_bar)) + geom_point(alpha = 0.8, shape = 21, size = 1.5)
 
-#weight search
+#windowing search
 
-tdfsum = mutate(tdf1,weight_mean = roll_mean(Pressure_Hydraulic_main_mast_bar,10,fill = numeric(0),align = "center"))
+#window width
+w_width = 10
+if(w_width%%2!=0)
+  print("Must be even!")
+
+tdfsum = mutate(
+  tdfsum,
+  weight_mean = c(rep(0,w_width/2),
+                  roll_mean(Pressure_Hydraulic_main_mast_bar,w_width,fill = numeric(0),align = "center"),
+                  rep(0,w_width/2-1)
+                  )
+                )
+
 length(tdfsum$Pressure_Hydraulic_main_mast_bar)
 length(roll_mean(tdfsum$Pressure_Hydraulic_main_mast_bar,10,fill = numeric(0),align = "center"))
 #coerce nulls
